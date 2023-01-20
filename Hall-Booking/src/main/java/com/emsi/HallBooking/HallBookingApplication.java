025264e83f6ed36f664fd6469585ce2f936ea8a3
YassineEMSI <etu_elkhamlichi.yassine@emsi-edu.ma>
2023-01-20 08:16:15 +0100

    Yassine fix 0.1

--- Hall-Booking/src/main/java/com/emsi/HallBooking/HallBookingApplication.java
+++ Hall-Booking/src/main/java/com/emsi/HallBooking/HallBookingApplication.java
@@ -2,12 +2,30 @@ package com.emsi.HallBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HallBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HallBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HallBookingApplication.class, args);
+    }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
--- Hall-Booking/src/main/java/com/emsi/HallBooking/controller/rest/BookingRestController.java
+++ Hall-Booking/src/main/java/com/emsi/HallBooking/controller/rest/BookingRestController.java
@@ -23,11 +23,6 @@ public class BookingRestController {
         this.bookingService = bookingService;
     }
 
-    @GetMapping(value = "/rest/booking")
-    public List<BookingVo> getAll() {
-        return bookingService.getBookings();
-    }
-
     @GetMapping("")
     public ResponseEntity<Object> getAllBookings() {
         List<BookingVo> bookingVos = bookingService.getBookings();
@@ -36,54 +31,54 @@ public class BookingRestController {
         return new ResponseEntity<>(bookingVos, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/rest/booking/{id}")
-    public ResponseEntity<Object> getBookingById(@PathVariable(value = "id") Long bookingId) {
-        BookingVo bookingVoFound = bookingService.getBookingById(bookingId);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getBookingById(@PathVariable(value = "id") Long bookingVoId) {
+        BookingVo bookingVoFound = bookingService.getBookingById(bookingVoId);
         if (bookingVoFound == null)
-            return new ResponseEntity<>("booking doesn't exist", HttpStatus.OK);
+            return new ResponseEntity<>("booking doesn't exist", HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(bookingVoFound, HttpStatus.OK);
     }
 
 
-    @GetMapping("/booking/search")
+    @GetMapping("/search")
     public ResponseEntity<Object> searchBooking(@RequestParam("date") Date date, @RequestParam("idHall") Long idHall, @RequestParam("idClient") Long idClient) {
-        List<BookingVo> bookingVo = bookingService.search(date, idHall, idClient);
-        if (bookingVo == null)
-            return new ResponseEntity<>("booking does not exit", HttpStatus.OK);
-        return new ResponseEntity<>(bookingVo, HttpStatus.OK);
+        List<BookingVo> bookingsVo = bookingService.search(date, idHall, idClient);
+        if (bookingsVo == null)
+            return new ResponseEntity<>("booking does not exit", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(bookingsVo, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/rest/booking/create")
+    @PostMapping(value = "/add")
     public ResponseEntity<Object> createBooking(@Valid @RequestBody BookingVo bookingVo) {
         bookingService.save(bookingVo);
         return new ResponseEntity<>("booking is created successfully", HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/rest/booking/update/{id}")
+    @PutMapping(value = "/update/{id}")
     public ResponseEntity<Object> updateBooking(@PathVariable(name = "id") Long bookingId, @RequestBody BookingVo bookingVo) {
         BookingVo bookingFound = bookingService.getBookingById(bookingId);
         if (bookingFound == null)
-            return new ResponseEntity<>("Booking doesn't exist", HttpStatus.OK);
+            return new ResponseEntity<>("Booking doesn't exist", HttpStatus.NOT_FOUND);
         bookingVo.setIdBooking(bookingId);
         bookingService.save(bookingVo);
         return new ResponseEntity<>("Booking is updated successsfully", HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/rest/booking/delete/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Object> deleteBooking(@PathVariable(name = "id") Long bookingId) {
         BookingVo bookingVoFound = bookingService.getBookingById(bookingId);
         if (bookingVoFound == null)
-            return new ResponseEntity<>("Booking doesn't exist", HttpStatus.OK);
+            return new ResponseEntity<>("Booking doesn't exist", HttpStatus.NOT_FOUND);
         bookingService.delete(bookingId);
         return new ResponseEntity<>("Booking is deleted successfully", HttpStatus.OK);
     }
 
-    @GetMapping(value = "/rest/sort/{fieldName}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @GetMapping(value = "/sort/{fieldName}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public List<BookingVo> sortBy(@PathVariable String fieldName) {
         return bookingService.sortBy(fieldName);
     }
 
-    @GetMapping("/rest/pagination/{pageId}/{size}")
+    @GetMapping("/page/{pageId}/{size}")
     public List<BookingVo> pagination(@PathVariable int pageId, @PathVariable int size, Model m) {
         return bookingService.findAll(pageId, size);
     }
